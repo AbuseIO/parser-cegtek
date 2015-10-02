44229f8d7fd90ec6371d14a7e686d9feed43ab5b
Bart Vrancken <bart@abuse.io>
2015-10-02 22:08:31 +0200

    update new layout

--- src/Cegtek.php
+++ src/Cegtek.php
@@ -5,21 +5,16 @@ namespace AbuseIO\Parsers;
 use Ddeboer\DataImport\Reader;
 use Ddeboer\DataImport\Writer;
 use Ddeboer\DataImport\Filter;
-use Log;
-use ReflectionClass;
 
 class Cegtek extends Parser
 {
-    public $parsedMail;
-    public $arfMail;
-
     /**
      * Create a new Cegtek instance
      */
     public function __construct($parsedMail, $arfMail)
     {
-        $this->parsedMail = $parsedMail;
-        $this->arfMail = $arfMail;
+        // Call the parent constructor to initialize some basics
+        parent::__construct($parsedMail, $arfMail, $this);
     }
 
     /**
@@ -29,20 +24,6 @@ class Cegtek extends Parser
      */
     public function parse()
     {
-        // Generalize the local config based on the parser class name.
-        $reflect = new ReflectionClass($this);
-        $configBase = 'parsers.' . $reflect->getShortName();
-
-        Log::info(
-            get_class($this) . ': Received message from: ' .
-            $this->parsedMail->getHeader('from') . " with subject: '" .
-            $this->parsedMail->getHeader('subject') . "' arrived at parser: " .
-            config("{$configBase}.parser.name")
-        );
-
-        $events = [ ];
-        $report = [ ];
-
         $this->feedName = 'default';
 
         // XML is placed in the body
@@ -52,60 +33,53 @@ class Cegtek extends Parser
             $regs
         )) {
             $report = $regs[0];
-        }
 
-        if (!$this->isKnownFeed()) {
-            return $this->failed(
-                "Detected feed {$this->feedName} is unknown."
-            );
-        }
+            if (!empty($report) && $report = simplexml_load_string($report)) {
 
-        if (!$this->isEnabledFeed()) {
-            return $this->success($events);
-        }
+                if ($this->isKnownFeed() && $this->isEnabledFeed()) {
 
-        if (!$this->hasRequiredFields($report)) {
-            return $this->failed(
-                "Required field {$this->requiredField} is missing or the config is incorrect."
-            );
-        }
+                    /*
+                     * Work around the crappy timestamp used by IP-echelon, i.e.: 2015-05-06T05-00-00UTC
+                     * We loose some timezone information, but hey it's close enough ;)
+                     */
+                    if (preg_match(
+                        '/^([0-9-]+)T([0-9]{2})-([0-9]{2})-([0-9]{2})/',
+                        $report->Source->TimeStamp,
+                        $regs
+                    )) {
+                        $timestamp = strtotime($regs[1].' '.$regs[2].':'.$regs[3].':'.$regs[4]);
+                        // Fall back to now if we can't parse the timestamp
+                    } else {
+                        $timestamp = time();
+                    }
 
-        if (!empty($report) && $report = simplexml_load_string($report)) {
-            // Work around the crappy timestamp used by IP-echelon, i.e.: 2015-05-06T05-00-00UTC
-            // We loose some timezone information, but hey it's close enough ;)
-            if (preg_match('/^([0-9-]+)T([0-9]{2})-([0-9]{2})-([0-9]{2})/', $report->Source->TimeStamp, $regs)) {
-                $timestamp = strtotime($regs[1].' '.$regs[2].':'.$regs[3].':'.$regs[4]);
-                // Fall back to now if we can't parse the timestamp
+                    /*
+                     * The XML contains so many crap you cant even think about filters here so we grab
+                     * the fields ourselves.
+                     */
+                    $infoBlob = [
+                        'type'          => (string)$report->Source->Type,
+                        'port'          => (string)$report->Source->Port,
+                        'number_files'  => (string)$report->Source->Number_Files,
+                        'complainant'   => (string)$report->Complainant->Entity,
+                    ];
+
+                    $this->events[] = [
+                        'source'        => config("{$this->configBase}.parser.name"),
+                        'ip'            => (string)$report->Source->IP_Address,
+                        'domain'        => false,
+                        'uri'           => false,
+                        'class'         => config("{$this->configBase}.feeds.{$this->feedName}.class"),
+                        'type'          => config("{$this->configBase}.feeds.{$this->feedName}.type"),
+                        'timestamp'     => $timestamp,
+                        'information'   => json_encode($infoBlob),
+                    ];
+                }
             } else {
-                $timestamp = time();
+                $this->warningCount++;
             }
-
-            // The XML contains so many crap you cant even think about filters here so we grab the fields ourselves.
-            $infoBlob = [
-                'type'          => (string)$report->Source->Type,
-                'port'          => (string)$report->Source->Port,
-                'number_files'  => (string)$report->Source->Number_Files,
-                'complainant'   => (string)$report->Complainant->Entity,
-            ];
-
-            $event = [
-                'source'        => config("{$configBase}.parser.name"),
-                'ip'            => (string)$report->Source->IP_Address,
-                'domain'        => false,
-                'uri'           => false,
-                'class'         => config("{$configBase}.feeds.{$this->feedName}.class"),
-                'type'          => config("{$configBase}.feeds.{$this->feedName}.type"),
-                'timestamp'     => $timestamp,
-                'information'   => json_encode($infoBlob),
-            ];
-
-            $events[] = $event;
-        } else {
-            return $this->failed(
-                "Unable to get a valid XML."
-            );
         }
 
-        return $this->success($events);
+        return $this->success();
     }
 }
